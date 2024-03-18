53f61c87b5df8d42385aa330fd7bf3c0f573b077
RivaPermana <161426135+RivaPermana@users.noreply.github.com>
2024-03-18 07:13:37 +0700

    Add files via upload
    first

---
+++ modul5_1302223010/modul5_1302223010/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Penjumlahan
+{
+    public void JumlahTigaAngka()
+    {
+        dynamic 
+    }
+    public static void Main(string[] args)
+    {
+
+    }
+}
+
